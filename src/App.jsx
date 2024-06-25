dbd8fac9db76d04b3ad2790800b3785c2f821a2f
aicha-azr <aichaazeroual03@gmail.com>
2024-06-25 18:04:39 +0100

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <> 
   
-   <main className='fixed w-full max-w-fit h-[100vh] border border-black left-0 right-0 top-0 overflow-y-auto scroll-smooth px-2 '>
+   <main className='fixed w-full max-w-fit h-[100vh] border border-black left-0 right-0 top-0 overflow-y-auto scroll-smooth px-0.5 lg:px-2'>
     <SideBar/>
 
   <Home/>
